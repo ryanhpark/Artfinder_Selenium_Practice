01066b9b6cde69237bfc6700cd08289228735f0a
Lucas <kimlucas92@gmail.com>
2020-10-01 13:42:57 -0300

    Scraping iteration 2

--- Scraper/artists/artists/settings.py
+++ Scraper/artists/artists/settings.py
@@ -65,7 +65,7 @@ DOWNLOAD_DELAY = 2
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'countryEarnings.pipelines.WriteItemPipeline': 300,
+    'artists.pipelines.WriteItemPipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
