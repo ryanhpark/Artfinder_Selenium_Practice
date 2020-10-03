63054039360332b121aa695a072a98ab849a4beb
Sita Thomas <sita.thomas@gmail.com>
2020-10-03 16:59:01 -0400

    Sita - add MaW scraper. WIP - only runs for single artist

--- Sita/scraper.py
+++ Sita/scraper.py
@@ -1,70 +1,91 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import time
 import numpy as np
+# from collections import defaultdict
 import pandas as pd
 
-# open webpage
+# get a list of all slugs
+artists = pd.read_csv("../artists.csv", usecols=['slug']).drop_duplicates()
+slugs = artists.slug.to_list()
+
+# establish driver
 driver = webdriver.Firefox(executable_path='C:\Program Files\geckodriver.exe')
-driver.get('https://www.artfinder.com/artist/aarti-bartake/me-at-work/#/')
 
-# close pop-up
-close_pop_up = driver.find_element_by_xpath('//div[@class="af-register-modal--b"]//a[@class="close"]')
-close_pop_up.click()
-time.sleep(2)
+# store strings from each page for each artist
+titles = []
+texts = []
+dates = []
+slugs = []
 
-# scroll to bottom to get all HTML elements
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-# get total number of relevant pages
-tot_pages = driver.find_element_by_xpath('//ul[@class="af-pagination "]').text[-1]
-
-# Artist 'Me at Work' page, save scraped text in lists part 1/2
-image_titles = []
-video_titles = []
-caption_text = []
-caption_dates = []
-
-# get text from each 'Me at Work' page
-for page in np.arange(1, (int(tot_pages) + 1)):
-  driver.get(f'https://www.artfinder.com/artist/aarti-bartake/me-at-work/?page={page}#/')
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+for slug in slugs[0:4]:
+  print(f'https://www.artfinder.com/artist/{slug}/me-at-work/#/')
+  # open webpage
+  driver.get(f'https://www.artfinder.com/artist/{slug}/me-at-work/#/')
   time.sleep(2)
 
-  # Artist 'Me at Work' page, scrape text
-  image = driver.find_elements_by_xpath('//summary/p[@class="margin margin-m af-bold"]')
-  video = driver.find_elements_by_xpath('//summary/h4[@class="margin margin-m"]')
-  text = driver.find_elements_by_xpath('//div/summary/footer/div[@class="margin margin-m margin-bottom"]')
-  dates = driver.find_elements_by_xpath('//div/summary/footer/p[@class="af-tiny-text af-picasso50-text"]')
+  # close pop-up
+  close_pop_up = driver.find_element_by_xpath('//div[@class="af-register-modal--b"]//a[@class="close"]')
+  try:
+    close_pop_up.click()
+    time.sleep(2)
+  except:
+    print('No pop-up')
 
-  # Artist 'Me at Work' page, save scraped text in lists part 2/2
-  def make_lists(element, elem_list):
-    for value in element:
-      elem_list.append(value.text)
-  make_lists(image, image_titles)
-  make_lists(video, video_titles)
-  make_lists(text, caption_text)
-  make_lists(dates, caption_dates)
+  # scroll to bottom to access all HTML elements
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-  map(image_titles.append, image_titles)
-  map(video_titles.append, video_titles)
-  map(caption_text.append, caption_text)
-  map(caption_dates.append, caption_dates)
+  # check for multiple pages
+  try:
+    # if there are multiple pages
+    driver.find_element_by_xpath('//ul[@class="af-pagination "]')
+    # get total number of relevant pages
+    tot_pages = driver.find_element_by_xpath('//ul[@class="af-pagination "]').text[-1]
+  # if not multiple pages, use the number 1 for url
+  except NoSuchElementException:
+    tot_pages = '1'
 
+  print(tot_pages)
+  driver.close()
 
-# add lists to dictionary
-posts = {'image_titles': image_titles, 'video_titles': video_titles,
-         'caption_text': caption_text, 'caption_date': caption_dates}
-##### SAVE SLUG AS COLUMN FOR JOINING LATER #####
-##### SAVE SLUG AS COLUMN FOR JOINING LATER #####
-##### SAVE SLUG AS COLUMN FOR JOINING LATER #####
+  # get posts from each 'Me at Work' page
+  for page in np.arange(1, (int(tot_pages) + 1)):
+    driver.get(f'https://www.artfinder.com/artist/{slug}/me-at-work/?page={page}#/')
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
+    # get post titles, texts, and dates
+    posts = driver.find_elements_by_xpath('//summary')
+    posts = [post.text.split('\n') for post in posts]
+
+    print(posts)
+
+    # if text is missing, insert 'N/A' string where text should be
+    for post in posts:
+      if len(post) == 2:
+        post.insert(1, 'N/A')
+      # add strings to storage outside loop
+      titles.append(post[0])
+      texts.append(post[1])
+      dates.append(post[2])
+      slugs.append(slug)
+
+      print(titles)
+      print(texts)
+      print(dates)
+      print(slugs)
+
+    driver.close()
+
+# add stored lists to dictionary
+posts = {'post_title': titles, 'post_text': texts,
+         'post_date': dates, 'slug': slugs}
+
+print(posts)
 
 # convert dictionary to df and export as .csv
 df = pd.DataFrame.from_dict(posts, orient='index')
 df_final = df.transpose()
 df_final.to_csv('posts.csv', index=False, header=True, encoding='utf-8')
 
-
-driver.close()
+driver.quit()
