f7da97ca75fecfebb2630237f83bc50d20943b9c
Sita Thomas <sita.thomas@gmail.com>
2020-10-03 20:07:13 -0400

    Sita - take 2... Me at Work scraper final (pre-scrape)

--- Sita/scraper.py
+++ Sita/scraper.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
 import numpy as np
@@ -10,6 +13,7 @@ artists = pd.read_csv("../artists.csv", usecols=['slug']).drop_duplicates()
 slugs_list = artists['slug'].to_list()
 
 # store strings from each page for each artist
+video = []
 titles = []
 texts = []
 dates = []
@@ -18,57 +22,75 @@ slugs = []
 # establish driver
 driver = webdriver.Firefox(executable_path='C:\Program Files\geckodriver.exe')
 
-for slug in slugs_list:
-  # open webpage
+for slug in slugs_list[19:26]:
+  # open artist's Me at Work page
   driver.get(f'https://www.artfinder.com/artist/{slug}/me-at-work/#/')
   time.sleep(2)
-
   # close pop-up
   try:
-    close_pop_up = driver.find_element_by_xpath('//div[@class="af-register-modal--b"]//a[@class="close"]')
+    close_pop_up = driver.find_element_by_xpath(
+      '//div[@class="af-register-modal--b"]//a[@class="close"]')
     close_pop_up.click()
-    time.sleep(2)
   except:
     pass
 
   # scroll to bottom to access all HTML elements
   driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,
+        '//div[@class="scroll-to-top af-opacity"]')))
+
 
   # check for multiple pages
   try:
     # if there are multiple pages
     driver.find_element_by_xpath('//ul[@class="af-pagination "]')
     # get total number of relevant pages
-    tot_pages = driver.find_element_by_xpath('//ul[@class="af-pagination "]').text[-1]
+    tot_pages = driver.find_element_by_xpath('//ul[@class="af-pagination "]'
+      ).text.split("\n")[-1]
   # if not multiple pages, use the number 1 for url
   except NoSuchElementException:
     tot_pages = '1'
 
-  # get posts from each 'Me at Work' page
-  for page in np.arange(1, (int(tot_pages) + 1)):
-    driver.get(f'https://www.artfinder.com/artist/{slug}/me-at-work/?page={page}#/')
-    time.sleep(3)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+  try:
+    # get posts from each 'Me at Work' page
+    for page in np.arange(1, (int(tot_pages) + 1)):
+      driver.get(f'https://www.artfinder.com/artist/{slug}/me-at-work/?page={page}#/')
+      driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+      WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH,
+        '//div[@class="scroll-to-top af-opacity"]')))
 
-    # get post titles, texts, and dates
-    posts = driver.find_elements_by_xpath('//summary')
-    posts = [post.text.split('\n') for post in posts]
+      # get post titles, texts, and dates
+      WebDriverWait(driver, 10).until(
+          EC.presence_of_element_located((By.XPATH, '//summary')))
+      posts = driver.find_elements_by_xpath('//summary')
+      posts = [post.text.split('\n') for post in posts]
 
-    # if text is missing, insert 'N/A' string where text should be
-    for post in posts:
-      if len(post) == 2:
-        post.insert(1, 'N/A')
-      if len(post) == 1:
-        post.insert(0, 'N/A')
-        post.insert(1, 'N/A')       
-      # add strings to storage outside loop
-      titles.append(post[0])
-      texts.append(post[1])
-      dates.append(post[2])
-      slugs.append(slug)
+      # if any text is missing, insert 'N/A'
+      for post in posts:
+        if len(post) == 2:
+          post.insert(0, 'N/A')
+        if len(post) == 1:
+          post.insert(0, 'N/A')
+          post.insert(1, 'N/A')
+        try:
+          driver.find_element_by_xpath(
+            '//div[@class="af-card  af-video-text-card text-left margin margin-bottom margin-s"]')
+          video.append(1)
+        except:
+          video.append(0)
+
+        # add strings to storage outside loop
+        titles.append(post[0])
+        texts.append(post[1])
+        dates.append(post[2])
+        slugs.append(slug)
+  except:
+    pass
 
 # add stored lists to dictionary
-posts = {'post_title': titles, 'post_text': texts,
+posts = {'is_video': video, 'post_title': titles, 'post_text': texts,
          'post_date': dates, 'slug': slugs}
 
 # convert dictionary to df and export as .csv
